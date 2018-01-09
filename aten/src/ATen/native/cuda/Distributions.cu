a9591dfb542123626bd3c7de715fad93c603ac1e
Rachit Singh <rachitsingh@outlook.com>
2018-01-09 11:14:47 +0000

    Ignore half issue -- to squash

--- aten/src/ATen/native/cuda/Distributions.cu
+++ aten/src/ATen/native/cuda/Distributions.cu
@@ -4,20 +4,11 @@
 #include <curand.h>
 #include <curand_kernel.h>
 
-#include <THC/THCTensorMath.h>
 #include <THC/THCGeneral.h>
 #include <THC/THCHalf.h>
-#include <THC/THCTensorCopy.h>
 #include <THC/THCApply.cuh>
 #include <THC/THCNumerics.cuh>
-#include <THC/THCReduce.cuh>
 #include <THC/THCTensorRandom.h>
-
-#include <THC/THCDeviceUtils.cuh>
-#include <THC/THCTensorMathReduce.cuh>
-#include <THC/THCTensorSort.cuh>
-#include <THC/THCThrustAllocator.cuh>
-
 
 THCGenerator* THCRandom_getGenerator(THCState* state);
 
@@ -40,12 +31,13 @@ namespace dist {
       );
     }
   };
+
 } // at::native::dist
 
 Tensor _s_poisson_cuda(const Tensor& lambda, Generator* gen) {
   Tensor ret = lambda.type().tensor(lambda.sizes());
   auto lambda_ = lambda.toType(ScalarType::Float);
-  dispatch_all<void, dist::PoissonOpCUDA>(ret.type(), "poisson", ret, lambda_, dist::get_states(gen));
+  dispatch_floating_types<void, dist::PoissonOpCUDA>(ret.type(), "poisson", ret, lambda_, dist::get_states(gen));
   return ret;
 }
 
